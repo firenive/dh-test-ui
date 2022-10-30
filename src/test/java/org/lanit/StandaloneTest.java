5814b7d23bb0913d123b388d45206f8777a49e61
firenive <48296956+firenive@users.noreply.github.com>
2022-10-30 20:51:26 +0300

    add work with products

--- src/test/java/org/lanit/StandaloneTest.java
+++ src/test/java/org/lanit/StandaloneTest.java
@@ -3,9 +3,11 @@ package org.lanit;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class StandaloneTest {
     public static void main(String[] args) {
@@ -14,10 +16,17 @@ public class StandaloneTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("http://docshouse-test.k8s.lan.lanit.ru/");
-//v-btn__content
+        //v-btn__content
         driver.findElement(By.cssSelector("input[type='text']")).sendKeys("user");
         driver.findElement(By.cssSelector("input[type='password']")).sendKeys("password");
 
         driver.findElement(By.cssSelector(".v-btn__content")).click();
+
+        // practise java stream
+        List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+        products.stream().filter(product -> product
+                .findElement(By.cssSelector("b"))
+                .getText().equals("ZaRA COAT 3"))
+                .findFirst().orElse(null);
     }
 }
